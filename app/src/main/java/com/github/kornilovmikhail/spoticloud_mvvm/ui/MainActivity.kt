924fafc058ffcc00522675e29653e4082b7c9f1c
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-08-13 01:00:04 +0400

    added skeleton

--- app/src/main/java/com/github/kornilovmikhail/spoticloud_mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/github/kornilovmikhail/spoticloud_mvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.kornilovmikhail.spoticloud_mvvm
+package com.github.kornilovmikhail.spoticloud_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kornilovmikhail.spoticloud_mvvm.R
 
 class MainActivity : AppCompatActivity() {
 
