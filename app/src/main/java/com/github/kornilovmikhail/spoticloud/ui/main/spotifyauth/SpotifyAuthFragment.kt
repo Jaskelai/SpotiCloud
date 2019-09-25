46ccb452da28aabb64d7afc06ca7ba19a0f220fa
KornilovMikhail <kornilovmisha1999@gmail.com>
2019-09-25 18:31:36 +0300

    fixed spotify auth for pre-N devices

--- app/src/main/java/com/github/kornilovmikhail/spoticloud/ui/main/spotifyauth/SpotifyAuthFragment.kt
+++ app/src/main/java/com/github/kornilovmikhail/spoticloud/ui/main/spotifyauth/SpotifyAuthFragment.kt
@@ -2,6 +2,7 @@ package com.github.kornilovmikhail.spoticloud.ui.main.spotifyauth
 
 import android.annotation.SuppressLint
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,7 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -28,7 +30,8 @@ class SpotifyAuthFragment : BaseFragment() {
         fun getInstance() = SpotifyAuthFragment()
     }
 
-    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
 
     private lateinit var spotifyAuthViewModel: SpotifyAuthViewModel
 
@@ -82,18 +85,32 @@ class SpotifyAuthFragment : BaseFragment() {
                 }
             }
 
+            @SuppressWarnings("deprecation")
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let {
+                    return dealWithUrl(Uri.parse(url))
+                }
+                return true
+            }
+
+            @RequiresApi(Build.VERSION_CODES.N)
             override fun shouldOverrideUrlLoading(
                 view: WebView?,
                 request: WebResourceRequest?
             ): Boolean {
                 request?.let {
-                    val code = request.url.getQueryParameter(CODE)
-                    spotifyAuthViewModel.onPageLoading(code)
-                    if (code != null) {
-                        return true
-                    }
+                    return dealWithUrl(it.url)
                 }
-                return super.shouldOverrideUrlLoading(view, request)
+                return true
+            }
+
+            private fun dealWithUrl(url: Uri): Boolean {
+                val code = url.getQueryParameter(CODE)
+                spotifyAuthViewModel.onPageLoading(code)
+                if (code != null) {
+                    return true
+                }
+                return false
             }
         }
     }
